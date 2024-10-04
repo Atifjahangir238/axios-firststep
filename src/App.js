fa6c87efb602ea704c511f0efcea620bc3762936
Atifjahangir238 <atifjahangir238@gmail.com>
2024-10-04 18:53:32 +0500

    real pro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.post('https://reqres.in/api/users', {
 
-export default App;
+      "name": "Atif",
+      "job": "Developer"
+
+    })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+    axios.delete('https://reqres.in/api/users/2')
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+    axios.put('https://reqres.in/api/users/2', {
+
+      "name": "kumail",
+      "job": "Bussiness Developer"
+
+    })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+
+    axios.get('https://reqres.in/api/users?page=2')
+    .then(res=>setData(res.data.data))
+    .catch(err => console.log(err));  },[])
+
+    return (
+      <div>
+        <h2>Axios Library in React JS</h2>
+        {
+          data.map((d, i) => {
+            return <>
+
+            <p key={i}>{d.email}</p>
+            <p key={i}>{d.avatar}</p>
+            </>
+          
+          })
+        }
+      </div>
+    )
+  }
+
+export default App
